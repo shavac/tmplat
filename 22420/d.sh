3157a58f92726b9c77d94800acd8d994eed5f4c6
freeidea <freeidea@live.cn>
2014-10-27 15:54:59 +0800

    Update d.sh

--- 22420/d.sh
+++ 22420/d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #name:cc.sh
 #将文件依据大小排序并输出
-find / -type f -exec ls -lS {} \; | awk 'BEGIN {
+find /usr/share/doc -type f -exec ls -lS {} \; | awk 'BEGIN {
  #得到第一行total总数并丢弃，读取下一行
  getline;getline;
  name1=$9;size=$5;
@@ -17,11 +17,10 @@ find / -type f -exec ls -lS {} \; | awk 'BEGIN {
   #如果校验和相同则为内容相同的文集，输出名字
   if( csum1==csum2 )
   {
-   {print name1, name2,size}
+   {print name1; print name2}
   }
  };
  size=$5;name1=name2;
 }' | sort -u > duplicate_files 
 #计算重复文件的md5sum，将重复文件中的一采样写入duplicate_sample中
-cat duplicate_files|xargs -I {} md5sum {}| sort | uniq -w 32 | awk '{print $2}' | sort -u > duplicate_sample 
-#print hehe
+cat duplicate_files|xargs -I {} md5sum {}| sort | uniq -w 32 | awk '{print $2}' | sort -u > duplicate_sample
